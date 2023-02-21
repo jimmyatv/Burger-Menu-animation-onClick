84eba01e7dd2db2541e7c7997c2a2576b00f2bda
jimmyatv <rasic.alexandar@gmail.com>
2023-02-21 19:43:20 +0100

    Alex's navBar with smooth animation - finished

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const burger = document.querySelector('.burger');
+const mobileNav = document.querySelector('.mobile-nav');
+
 
 //! without menyBar just onClick function
-burger.addEventListener('click', function () {
-    this.classList.toggle('is-active');
-});  
+// burger.addEventListener('click', function () {
+//     this.classList.toggle('is-active');
+// });  
+
+//* mobile-slide-bar
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    mobileNav.classList.toggle('is-active');
+})
